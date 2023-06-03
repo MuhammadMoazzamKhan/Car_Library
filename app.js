c64e419122e63df06cb7696bb2e09c58bcf97345
MuhammadMoazzamKhan <124440588+MuhammadMoazzamKhan@users.noreply.github.com>
2023-06-04 02:37:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,501 @@
+const cars = {
+    list: [{
+        name: "civic",
+        image: "honda1.jpg",
+        price: "PKR 86.0 lacs - 1.02 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "BR-V",
+        image: "honda2.jpg",
+        price: "PKR 65.3 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "city",
+        image: "honda3.jpg",
+        price: "PKR 48.0 - 59.8 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "accord",
+        image: "honda4.jpg",
+        price: "PKR 1.55 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "HR-V",
+        image: "honda5.jpg",
+        price: "PKR 79.0 - 82.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "vezel",
+        image: "honda6.jpg",
+        price: "PKR 70.0 lacs - 1.39 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "CR-V",
+        image: "honda7.jpg",
+        price: "PKR 47.8 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "fit",
+        image: "honda8.jpg",
+        price: "PKR 47.8 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "freed",
+        image: "honda9.jpg",
+        price: "PKR 22.0 - 30.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "insight",
+        image: "honda10.jpg",
+        price: "PKR 70.0 - 76.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "inspire",
+        image: "honda11.png",
+        price: "PKR 3.2 crore ",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "jazz",
+        image: "honda12.png",
+        price: "PKR 1.3 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "mobilio",
+        image: "honda13.png",
+        price: "PKR 95.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "n box",
+        image: "honda14.jpg",
+        price: "PKR 26.5 - 30.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "n one",
+        image: "honda15.jpg",
+        price: "PKR 39.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "n wgn",
+        image: "honda16.jpg",
+        price: "PKR 35.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "odyssey",
+        image: "honda17.jpg",
+        price: "PKR 2.44 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "passport",
+        image: "honda18.png",
+        price: "PKR 2.5 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "S660",
+        image: "honda19.jpg",
+        price: "PKR 27.4 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "spike",
+        image: "honda20.jpg",
+        price: "PKR 73.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "brio",
+        image: "honda21.jpg",
+        price: "PKR 23.6 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "elysion",
+        image: "honda22.jpg",
+        price: "PKR 3.0 crore",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "n-van",
+        image: "honda23.jpg",
+        price: "PKR 70.0 lacs",
+        category: "honda",
+        logo: "Hondalogo.png"
+    },
+    {
+        name: "corola",
+        image: "honda1.jpg",
+        price: "PKR 61.7 - 78.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "c-hr",
+        image: "toyota2.jpg",
+        price: "PKR 42.0 - 75.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "vitz",
+        image: "toyota3.jpg",
+        price: "PKR 23.0 - 43.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "yaris",
+        image: "toyota4.jpg",
+        price: "PKR 45.0 - 59.7 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "fortuner",
+        image: "toyota5.png",
+        price: "PKR 1.58 - 2.11 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "land cruiser",
+        image: "toyota6.png",
+        price: "PKR 15.68 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "alphard",
+        image: "toyota7.png",
+        price: "PKR 2.0 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "aqua",
+        image: "toyota8.jpg",
+        price: "PKR 45.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "auris",
+        image: "toyota9.png",
+        price: "PKR 22.0 - 30.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "camry",
+        image: "toyota10.jpg",
+        price: "PKR 5.39 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "coaster",
+        image: "toyota11.png",
+        price: "PKR 2.68 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "cross",
+        image: "toyota12.jpg",
+        price: "PKR 70.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "harrier",
+        image: "toyota13.jpg",
+        price: "PKR 7.82 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "hilux",
+        image: "toyota14.png",
+        price: "PKR 1.14 - 1.62 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "kluger",
+        image: "toyota15.png",
+        price: "PKR 1.64 crore ",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "noah",
+        image: "toyota16.png",
+        price: "PPKR 1.25 crore ",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "pixis epoch",
+        image: "toyota17.jpg",
+        price: "PKR 16.0 - 22.0 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "prado",
+        image: "toyota18.jpg",
+        price: "PKR 5.91 - 9.56 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "prius",
+        image: "toyota19.jpg",
+        price: "PKR 1.47 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "rav 4",
+        image: "toyota20.jpg",
+        price: "PKR 92.4 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    },
+    {
+        name: "rush",
+        image: "toyota21.jpg",
+        price: "PKR 80.1 - 83.3 lacs",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    }
+        ,
+    {
+        name: "supra",
+        image: "toyota22.png",
+        price: "PKR 1.53 crore",
+        category: "toyota",
+        logo: "Toyotalogo.png"
+    }
+        ,
+    {
+        name: "alto",
+        image: "suzuki1.png",
+        price: "PKR 22.5 - 29.4 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "cultus",
+        image: "suzuki2.png",
+        price: "PPKR 42.6 - 49.6 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "swift",
+        image: "suzuki3.jpg",
+        price: "PKR 32.1 - 37.4 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "wagon R",
+        image: "suzuki4.jpg",
+        price: "PKR 1.8 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "FX",
+        image: "suzuki5.jpg",
+        price: "PKR 1.53 crore",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "bolan",
+        image: "suzuki6.jpg",
+        price: "PKR 19 lakhs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "ignis",
+        image: "suzuki7.jpg",
+        price: "PKR 27.5 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "landy",
+        image: "suzuki8.jpg",
+        price: "PKR 1.53 crore",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "solio",
+        image: "suzuki9.jpg",
+        price: "PKR 1.53 crore",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "spacia",
+        image: "suzuki10.png",
+        price: "PKR 23.0 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "liana",
+        image: "suzuki11.jpg",
+        price: "PKR 8.4 - 11.9 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "Xbee",
+        image: "suzuki12.jpg",
+        price: "PKR 35.0 - 36.0 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "S cross",
+        image: "suzuki13.jpg",
+        price: "PKR 45.0 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "ertiga",
+        image: "suzuki14.jpg",
+        price: "PKR 76.0 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "every",
+        image: "suzuki15.jpg",
+        price: "PKR 2,400,000",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }
+        ,
+    {
+        name: "ravi",
+        image: "suzuki16.jpg",
+        price: "PKR 18.6 lacs",
+        category: "suzuki",
+        logo: "Suzukilogo.png"
+    }]
+}
+const button = document.querySelectorAll(".changing")
+
+const container = document.getElementsByClassName("row")[1];
+
+for (let i of cars.list) {
+    const showtext = cars.list.map((i) => {
+        return `
+            <div class="card mx-4 my-4 ${i.category}" style="width: 18rem;">
+            <img src="${i.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title headingColor carName">${i.name.toUpperCase()}</h5>
+                <p class="card-text priceColor">${i.price}</p>
+                <img src="${i.logo}" width="60" height="70">
+            </div>
+            </div>
+            `
+    })
+    let comma = showtext.join("")
+    container.innerHTML = comma
+}
+
+function buttons(value) {
+    let elements = document.querySelectorAll(".card")
+
+    elements.forEach((element) => {
+        if (value == "all") {
+            element.classList.remove("hide")
+        } else {
+            if (element.classList.contains(value)) {
+                element.classList.remove("hide")
+            } else {
+                element.classList.add("hide")
+            }
+        }
+    })
+
+}
+
+document.getElementById("input-btn").addEventListener("click", () => {
+    let searchInput = document.getElementById("search").value;
+    let elements = document.querySelectorAll(".carName");
+    let cards = document.querySelectorAll(".card");
+
+    elements.forEach((element, index) => {
+        if (element.innerText.includes(searchInput.toUpperCase())) {
+            cards[index].classList.remove("hide");
+        } else {
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+window.onload = ()=>{
+    buttons('all')
+}
